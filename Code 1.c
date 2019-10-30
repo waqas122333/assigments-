1c508c9c7e97d8a20b849156bfbcd30ed6600989
waqas122333 <56858282+waqas122333@users.noreply.github.com>
2019-10-30 23:04:14 +0500

    Add files via upload

---
+++ Code 1.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+
+int main() 
+{ 
+float time,result; 
+int choice;
+ printf("\tenter time\t");
+ scanf("%f",&time);
+ printf("\n1=Hours to milliseconds");
+ printf("\n2=milliseconds to hours"); 
+printf("\n\tenter choice\t");
+ scanf("%d",&choice);
+ if(choice==1)
+ { 
+result=time*3600000;
+ } 
+else if(choice==2)
+ { 
+result=time/3600000;
+ }
+ printf("\n\n\tresult=%f",result);
+ return 0;
+ }
